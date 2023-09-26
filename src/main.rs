ea155a5e4ca7be9c899fc89647f7ca1cc0123337
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2023-09-26 22:31:13 +0200

    use file_lock for synchronization

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::fs;
 extern crate xdg;
 use xdg::BaseDirectories;
 
+extern crate file_lock;
+use file_lock::{FileLock, FileOptions};
+
 use std::path::PathBuf;
 
 use filetime::FileTime;
@@ -22,6 +25,10 @@ struct Args {
     // Should we cache results from failed commands
     #[arg(long, default_value_t = false)]
     cache_failures: bool,
+
+    // Verbose mode
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
 
     // The command to execute
     command: String,
@@ -53,28 +60,29 @@ fn get_cached_paths(
     dirs: &BaseDirectories,
     path: &PathBuf,
     encoded_args: &str,
-) -> (PathBuf, PathBuf, PathBuf) {
+) -> (PathBuf, PathBuf, PathBuf, PathBuf) {
+    let lockfile_path = get_cache_file_with_prefix("lockfile_".into(), &path, &encoded_args, dirs);
     let exitcode_path = get_cache_file_with_prefix("exitcode_".into(), &path, &encoded_args, dirs);
     let stdout_path = get_cache_file_with_prefix("stdout_".into(), &path, &encoded_args, dirs);
     let stderr_path = get_cache_file_with_prefix("stderr_".into(), &path, &encoded_args, dirs);
-    return (exitcode_path, stdout_path, stderr_path);
+    return (lockfile_path, exitcode_path, stdout_path, stderr_path);
 }
 
 fn get_cached_value<'a>(
     args: &Args,
-    paths: &'a (PathBuf, PathBuf, PathBuf),
+    paths: &'a (PathBuf, PathBuf, PathBuf, PathBuf),
 ) -> Option<(i32, &'a PathBuf, &'a PathBuf)> {
-    let meta = fs::metadata(&paths.0).ok()?;
+    let meta = fs::metadata(&paths.1).ok()?;
     let ftime = FileTime::from_last_modification_time(&meta);
     let now_time = FileTime::now();
 
     let time_since_modification = now_time.seconds() - ftime.seconds();
     if time_since_modification >= args.cache_seconds.into() {
-        // dbg!("Invalidated cache: is too old: {}", time_since_modification);
+        dbg!("Invalidated cache: is too old: {}", time_since_modification);
         return None;
     }
 
-    let exitcode = match fs::read_to_string(&paths.0) {
+    let exitcode = match fs::read_to_string(&paths.1) {
         Ok(exitcode_str) => match exitcode_str.parse::<i32>() {
             Ok(exitcode) => exitcode,
             Err(_) => return None,
@@ -84,16 +92,17 @@ fn get_cached_value<'a>(
 
     if exitcode != 0 && !args.cache_failures {
         // Command had non-zero exit code, and we don't cache that so ignore it
+        // dbg!("Command failed: {}", time_since_modification);
         return None;
     }
 
-    return Some((exitcode, &paths.1, &paths.2));
+    return Some((exitcode, &paths.2, &paths.3));
 }
 
 fn run_and_put_cached_value<'a>(
     dirs: &BaseDirectories,
     args: &Args,
-    paths: &'a (PathBuf, PathBuf, PathBuf),
+    paths: &'a (PathBuf, PathBuf, PathBuf, PathBuf),
 ) -> Option<(Option<i32>, &'a PathBuf, &'a PathBuf)> {
     let command_result = Command::new(args.command.clone())
         .args(args.command_args.clone())
@@ -104,32 +113,32 @@ fn run_and_put_cached_value<'a>(
         Err(_) => return None,
     };
 
-    if dirs.place_cache_file(&paths.0).is_err() {
-        return None;
-    }
     if dirs.place_cache_file(&paths.1).is_err() {
         return None;
     }
     if dirs.place_cache_file(&paths.2).is_err() {
         return None;
     }
+    if dirs.place_cache_file(&paths.3).is_err() {
+        return None;
+    }
 
     if let Some(exitcode) = result.status.code() {
-        if fs::write(&paths.0, exitcode.to_string()).is_err() {
+        if fs::write(&paths.1, exitcode.to_string()).is_err() {
             return None;
         };
     } else {
         return None;
     }
 
-    if fs::write(&paths.1, &result.stdout).is_err() {
+    if fs::write(&paths.2, &result.stdout).is_err() {
         return None;
     };
-    if fs::write(&paths.2, &result.stderr).is_err() {
+    if fs::write(&paths.3, &result.stderr).is_err() {
         return None;
     };
 
-    return Some((result.status.code(), &paths.1, &paths.2));
+    return Some((result.status.code(), &paths.2, &paths.3));
 }
 
 fn display_cached_values(stdout: &PathBuf, stderr: &PathBuf) -> Result<(), io::Error> {
@@ -156,19 +165,34 @@ fn main() {
     //         other => Err(other).expect("unable to create cache directories"),
     //     },
     // }
-    dbg!("cache_path: {:?}", &path);
+    if args.verbose {
+        println!("== cache_path: {:?}", &path);
+    }
 
     let encoded_args = encode_command_args(&args.command_args);
     let paths = get_cached_paths(&dirs, &path, &encoded_args);
 
+    // get a lock on the lock_file
+    let should_we_block = true;
+    let options = FileOptions::new().write(true).create(true).append(false);
+
+    let lock = FileLock::lock(&paths.0, should_we_block, options).expect("unable to get file lock");
+
     if let Some((exit_code, stdout, stderr)) = get_cached_value(&args, &paths) {
-        // println!("Using cached value: {:?}", (exit_code, stdout, stderr));
+        if args.verbose {
+            println!("== Using cached value: {:?}", (exit_code, stdout, stderr));
+        }
         display_cached_values(stdout, stderr).expect("unable to read and write from cache");
+        lock.unlock();
         std::process::exit(exit_code);
+    }
+    if args.verbose {
+        println!("== Running...");
     }
 
     if let Some((exit_code, stdout, stderr)) = run_and_put_cached_value(&dirs, &args, &paths) {
         display_cached_values(stdout, stderr).expect("unable to read and write from cache");
+        lock.unlock();
         std::process::exit(exit_code.unwrap_or(1));
     }
 }
